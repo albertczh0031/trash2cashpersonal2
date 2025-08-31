69ba78f24b44d870c6b8f0711229ed2d2159cd48
Chao Zhen Hao <zcha0031@student.monash.edu>
2025-09-01 00:25:52 +0800

    chore: edit the redis url to upstash

--- backend/trash2cash/trash2cash/settings.py
+++ backend/trash2cash/trash2cash/settings.py
@@ -227,7 +227,7 @@ LOGGING = {
 }
 
 # Celery (async task queue library)
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.getenv("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
