ec69ed011bf210ab29e57129e0df6ba81f8d68de
Chao Zhen Hao <zcha0031@student.monash.edu>
2025-09-02 20:12:14 +0800

    updating the django admin view

--- backend/trash2cash/rewards/admin.py
+++ backend/trash2cash/rewards/admin.py
@@ -3,5 +3,15 @@ from .models import Voucher
 from .models import VoucherInstance
 
 # admin.site.register(Voucher)
-admin.site.register(VoucherInstance)
-# Register your models here.
+
+from django import forms
+
+@admin.register(VoucherInstance)
+class VoucherInstanceAdmin(admin.ModelAdmin):
+    list_display = ("username", "voucher", "redeemed", "date",)
+    list_editable = ("voucher", "redeemed",)
+
+    def username(self, obj):
+        return obj.user.user.username
+    username.short_description = 'User'
+    username.admin_order_field = 'user__user__username'
